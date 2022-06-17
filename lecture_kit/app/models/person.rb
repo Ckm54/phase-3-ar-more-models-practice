7a4219ab92939bb849bf6f63357d33ff2f2376e4
Collins <mutaic84@gmail.com>
2022-06-18 00:41:32 +0300

    implement water plant method

--- lecture_kit/app/models/person.rb
+++ lecture_kit/app/models/person.rb
@@ -5,4 +5,16 @@ class Person < ActiveRecord::Base
 
     has_many :waterings
     has_many :waterees, through: :waterings
+
+    def water_plant(plant)
+        Watering.create(person_id: self.id, plant_id: plant.id)
+        parenthood = plant_parenthoods.find_by(plant: plant)
+        binding.pry
+        if parenthood
+            affection = parenthood.affection || 0exit
+            
+            affection += 1
+            parenthood.update(affection: affection)
+        end
+    end
 end
